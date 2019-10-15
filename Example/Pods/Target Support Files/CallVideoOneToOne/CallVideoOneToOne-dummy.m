0ae82ec964552f9c3cdb21e812a3be4b7dd602be
Phuoc <kieuvanphuoc277@gmail.com>
2019-10-15 23:19:43 +0700

    udpate adding file

---
+++ Example/Pods/Target Support Files/CallVideoOneToOne/CallVideoOneToOne-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CallVideoOneToOne : NSObject
+@end
+@implementation PodsDummy_CallVideoOneToOne
+@end
---
+++ Example/Pods/Target Support Files/Pods-CallVideoOneToOne_Example/Pods-CallVideoOneToOne_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CallVideoOneToOne_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CallVideoOneToOne_Example
+@end
